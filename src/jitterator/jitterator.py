cb3ef8f5d6723b38f0d46e3fb95d55745a6843ca
Jun Sun <yfiua@qq.com>
2023-11-05 21:21:11 +0100

    add multiplicative

--- src/jitterator/jitterator.py
+++ src/jitterator/jitterator.py
@@ -1,20 +1,39 @@
 import random
 
 def jitterate(jitter=0.05, mode='+'):
+
+    # mode: '+' for additive, '*' for multiplicative
+    if mode not in ['+', '*']:
+        raise ValueError('jitterate: mode must be either "+" or "*".')
+
     def decorator_with_arbitrary_arguments(function):
+
+        def is_additive(obj):
+            return hasattr(obj, '__add__') and callable(obj.__add__)
+
+        def is_multiplicative(obj):
+            return hasattr(obj, '__mul__') and callable(obj.__mul__)
+
         def wrapper_accepting_arguments(*args, **kwargs):
 #            print("kwargs:")
 #            for kwarg in kwargs:
 #                val = kwargs.get(kwarg)
 #                print(kwarg, type(kwarg), val, type(val))
 #
-
             jittered_args = list(args)
-            for i, arg in enumerate(args):
-                if isinstance(arg, (int, float)):
-                    jittered_args[i] += random.uniform(-jitter, jitter)
-                else:
-                    print(f'jitterator: arg {i} is not additive, skipping.')
+
+            if mode == '+':
+                for i, arg in enumerate(args):
+                    if is_additive(arg):
+                        jittered_args[i] += random.uniform(-jitter, jitter)
+                    else:
+                        print(f'jitterator: arg {i} is not additive, skipping.')
+            elif mode == '*':
+                for i, arg in enumerate(args):
+                    if is_multiplicative(arg):
+                        jittered_args[i] *= random.uniform(1-jitter, 1+jitter)
+                    else:
+                        print(f'jitterator: arg {i} is not multiplicative, skipping.')
 
             return function(*jittered_args, **kwargs), jittered_args
 
